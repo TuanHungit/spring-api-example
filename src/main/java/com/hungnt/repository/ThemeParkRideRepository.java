57b0fe997b1acc7ad44f1b5ed3e6ec6abf9ab5e5
HungNT <hungnt@splus-software.com.vn>
2021-11-16 19:50:17 +0700

    init project

---
+++ src/main/java/com/hungnt/repository/ThemeParkRideRepository.java
@@ -0,0 +1,11 @@
+package com.hungnt.repository;
+
+import com.hungnt.entity.ThemeParkRide;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+@Repository
+public interface ThemeParkRideRepository extends CrudRepository<ThemeParkRide, Long> {
+    List<ThemeParkRide> findByName(String name);
+}
