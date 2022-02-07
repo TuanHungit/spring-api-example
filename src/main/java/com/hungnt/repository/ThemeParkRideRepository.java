7866516c7e51c538805f5123d91d4f7c7ed362ac
hungnt <tuanhungit14@gmail.com>
2022-02-07 17:30:20 +0700

    fix bug login docker

--- src/main/java/com/hungnt/repository/ThemeParkRideRepository.java
+++ src/main/java/com/hungnt/repository/ThemeParkRideRepository.java
@@ -5,7 +5,10 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 @Repository
 public interface ThemeParkRideRepository extends CrudRepository<ThemeParkRide, Long> {
     List<ThemeParkRide> findByName(String name);
+    
+    Optional<ThemeParkRide> findById(Long id);
 }
